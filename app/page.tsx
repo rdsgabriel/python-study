edf25f7c7c7ee1e0c52b8590953fc659d5cfe222
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2024-10-04 19:35:02 -0300

    links

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,17 @@ export default function Home() {
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-semibold mb-6 text-center">Boca = Expert em Python</h1>
       <BlogPosts initialPosts={allPostsData} />
+
+      <h2 className='font-bold text-blue-600'>links:</h2>
+      <a className='hover:text-blue-600' href='https://pythonspot.com/' target="_blank">Pythonspot</a>
+      <br />
+      <a className='hover:text-blue-600' href="https://www.w3schools.com/python/" target="_blank">W3Schools de Python</a>
+      <br />
+      <a className='hover:text-blue-600' href="https://docs.python.org/pt-br/3/" target="_blank">Doc oficial em PT-BR</a>
+      <br />
+      <br className='sm:hidden' />
+      <h3 className='text-blue-600 mt-1'> e claro, pesquisa tudo no youtube e evita ao máximo usar IA (a menos que peça pra ela gerar exercícios sobre o conteúdo).</h3>
+
     </div>
   )
 }
